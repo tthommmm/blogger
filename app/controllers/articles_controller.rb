30c0b608cd1c936b0926ca0b440d73ee8f8c6165
Thomas Blevins <38021483+tthommmm@users.noreply.github.com>
2020-02-15 13:57:07 -0500

    first commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,57 @@
+class ArticlesController < ApplicationController
+	def new
+		@article = Article.new
+		#hi
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+		#hi
+	end
+
+	def create
+	    @article = Article.new(article_params)
+	 
+	    if @article.save
+	      redirect_to @article
+	    else
+	      render 'new'
+	  end
+	end
+
+	def update
+	    @article = Article.find(params[:id])
+	 
+	    if @article.update(article_params)
+	      redirect_to @article
+	    else
+	      render 'edit'
+	  end
+	end
+
+	def destroy
+	    @article = Article.find(params[:id])
+		@article.destroy
+		 
+		redirect_to articles_path
+	end
+
+	def show
+		@article = Article.find(params[:id])
+		#out of all the articles we have, we want to find the ones with params of id which we get from
+	end
+
+	def index 
+		@articles = Article.all
+				#render plain: params[:article].inspect
+		#'plain' => 'params[:article]'
+		#this action gets information from POST 
+		#params[:title]
+		#params[:text]
+	end
+
+	private def article_params
+		params.require(:article).permit(:title, :text)
+		#require article, but only permite title and text parameters
+	end
+end
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,5 @@
+class PostsController < ApplicationController
+	def index
+		@title = "this is the index"
+	end
+end
